628ffbed8918de6e14eef58c10dda1ac2a1f08ab
ChatTeach <111213933+ChatTeach@users.noreply.github.com>
2023-07-03 19:01:37 +0530

    Add files via upload

---
+++ g4f/Provider/__init__.py
@@ -0,0 +1,29 @@
+from . import Provider
+from .Providers import (
+    Aichat,
+    Bard,
+    Bing,
+    ChatgptAi,
+    ChatgptLogin,
+    ChatgptLogin,
+    DeepAi,
+    Easychat,
+    Ezcht,
+    Fakeopen,
+    Forefront,
+    GetGpt,
+    Gravityengine,
+    H2o,
+    hteyun,
+    Liaobots,
+    Lockchat,
+    Mishalsgpt,
+    Phind,
+    Theb,
+    Vercel,
+    Weuseing,
+    Yqcloud,
+    You,
+)
+
+Palm = Bard
