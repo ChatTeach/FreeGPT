5c153b2158785320664edfaa48180a9a9e4012ee
ChatTeach <111213933+ChatTeach@users.noreply.github.com>
2023-08-25 21:56:26 +0530

    Delete g4f directory

--- g4f/README.md
+++
@@ -1,5 +0,0 @@
-## 🚀 API G4F
-
-This API is built upon the [gpt4free](https://github.com/xtekky/gpt4free) project.
-
-
--- g4f/typing.py
+++
@@ -1,3 +0,0 @@
-from typing import Dict, NewType, Union, Optional, List, get_type_hints
-
-sha256 = NewType('sha_256_hash', str)
